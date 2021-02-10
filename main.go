1ca81ef8332a5ced69b2638f22eae02d400c8267
maria.battaglia <maria.battaglia@globant.com>
2021-02-10 14:41:42 -0300

    create get task by id

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -31,6 +32,8 @@ func main() {
 	r.HandleFunc("/", homeHandler)
 	r.HandleFunc("/task", taskHandler).Methods("GET")
 	r.HandleFunc("/task", createTask).Methods("POST")
+	r.HandleFunc("/task/{id}", getTask).Methods("GET")
+	r.HandleFunc("/task/{id}", deleteTask).Methods("DELETE")
 
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", r))
@@ -40,6 +43,8 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to my API")
 
 }
+
+// CompileDaemon --command="./ApiRest.exe"
 
 func taskHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
@@ -59,8 +64,30 @@ func createTask(w http.ResponseWriter, r *http.Request) {
 	newTask.ID = len(tasks) + 1
 	tasks = append(tasks, newTask)
 
-	w.Header().Set("content-type", "application/json")
+	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newTask)
+
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		fmt.Print("ID invalid")
+		return
+	}
+
+	for _, task := range tasks {
+		if task.ID == taskID {
+			w.Header().Set("Content-type", "application/json")
+
+			json.NewEncoder(w).Encode(task)
+		}
+	}
 
 }

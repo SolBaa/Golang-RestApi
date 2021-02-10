d810f825434be98b871b77ca339c532953b98421
maria.battaglia <maria.battaglia@globant.com>
2021-02-10 14:32:46 -0300

    create function to create tasks

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -27,7 +29,9 @@ var tasks = allTasks{
 func main() {
 	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/", homeHandler)
-	r.HandleFunc("/task", taskHandler)
+	r.HandleFunc("/task", taskHandler).Methods("GET")
+	r.HandleFunc("/task", createTask).Methods("POST")
+
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
@@ -38,5 +42,25 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func taskHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to my Taks")
+	w.Header().Set("content-type", "application/json")
+
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask task
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Print("Insert a Valid Task!")
+	}
+	json.Unmarshal(reqBody, &newTask)
+
+	newTask.ID = len(tasks) + 1
+	tasks = append(tasks, newTask)
+
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+
 }

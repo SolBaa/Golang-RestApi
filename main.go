5bd823f595cdaa803d51914965583c0f5ff63260
maria.battaglia <maria.battaglia@globant.com>
2021-02-10 14:07:49 -0300

    erver is up

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type task struct {
+	ID      int    `json:ID`
+	Name    string `json:Name`
+	Content string `json:Name`
+}
+
+type allTasks []task
+
+var tasks = allTasks{
+	{
+		ID:      1,
+		Name:    "Task one",
+		Content: "Some content",
+	},
+}
 
 func main() {
-	fmt.Println("Hello")
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/task", taskHandler)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to my API")
+
+}
+
+func taskHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to my Taks")
 }
